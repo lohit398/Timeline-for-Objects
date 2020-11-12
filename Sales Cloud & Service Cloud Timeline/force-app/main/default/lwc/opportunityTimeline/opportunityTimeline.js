f276b75a901e9b88abed8fcccb5ffb02558bdac0
Lohit Botta <lohit.b@lohitb-ltm.internal.salesforce.com>
2020-11-12 15:49:28 +0530

    Prototype of Opportunity time line included

--- Sales Cloud & Service Cloud Timeline/force-app/main/default/lwc/opportunityTimeline/opportunityTimeline.js
+++ Sales Cloud & Service Cloud Timeline/force-app/main/default/lwc/opportunityTimeline/opportunityTimeline.js
@@ -1,3 +1,19 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,api,wire } from 'lwc';
+import getOppotunitiesData from '@salesforce/apex/CustomerInteractionTimeline_helper.getOppotunitiesData';
 
-export default class OpportunityTimeline extends LightningElement {}
+export default class OpportunityTimeline extends LightningElement {
+
+    @api recordId;
+    oppData={};
+
+    @wire(getOppotunitiesData, {recordId: '$recordId'})
+    opportuntyDetails({error,data}){
+        if(data){
+            this.oppData = data;
+            console.log(this.oppData);
+        }
+        else if(error){
+            console.log(error);
+        }
+    }
+}
