a578ef0bec403610936d1ee4764f671cafb15f8c
Lohit Botta <lohit.b@lohitb-ltm.internal.salesforce.com>
2020-12-10 16:44:29 +0530

    Handled : component not available

---
+++ Sales Cloud & Service Cloud Timeline/force-app/main/default/lwc/cit_illustration/cit_illustration.js
@@ -0,0 +1,5 @@
+import { LightningElement,api } from 'lwc';
+
+export default class Cit_illustration extends LightningElement {
+    @api objectName = '';
+}
--- Sales Cloud & Service Cloud Timeline/force-app/main/default/lwc/customerInteractionTimelineContainer/customerInteractionTimelineContainer.js
+++ Sales Cloud & Service Cloud Timeline/force-app/main/default/lwc/customerInteractionTimelineContainer/customerInteractionTimelineContainer.js
@@ -7,7 +7,9 @@ export default class CustomerInteractionTimelineContainer extends LightningEleme
     _isLead = false;
     _isOpportunity = false;
     _isSCObj = false;
-
+    _wrongObject = false;
+    __object;
+    __objectName;
     @api recordId;
 
 
@@ -17,31 +19,36 @@ export default class CustomerInteractionTimelineContainer extends LightningEleme
             console.log(error);
         }
         else if (data) {
-            if (data === 'Account') {
-                isPersonAccount({recordId: this.recordId})
-                .then(response => {
-                    if(response){
-                        this._isAccount = false;
-                        this._isSCObj = true
-                    }
-                    else{
-                        this._isAccount = true;
-                    }
-                })
-                .catch(error => {
-                    console.log(error);
-                })
-                
+            this.__object = data.apiName;
+            this.__objectName = data.objectName;
+            if (this.__object === 'Account') {
+                isPersonAccount({ recordId: this.recordId })
+                    .then(response => {
+                        if (response) {
+                            this._isAccount = false;
+                            this._isSCObj = true
+                        }
+                        else {
+                            this._isAccount = true;
+                        }
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    })
+
             }
-            else if (data === 'Case' || data === 'LiveChatTranscript' || data === 'Task' || data === 'Contact' || data === 'SocialPost' || data === 'Lead') {
+            else if (this.__object === 'Case' || this.__object === 'LiveChatTranscript' || this.__object === 'Task' || this.__object === 'Contact' || this.__object === 'SocialPost' || this.__object === 'Lead') {
                 this._isSCObj = true;
-                if(data === 'Lead')
+                if (this.__object === 'Lead')
                     this._isLead = true;
-                else 
+                else
                     this._isLead = false;
             }
-            else if (data === 'Opportunity') {
+            else if (this.__object === 'Opportunity') {
                 this._isOpportunity = true;
+            }
+            else {
+                this._wrongObject = true;
             }
         }
     }
